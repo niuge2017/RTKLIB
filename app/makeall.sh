20481884d40180395455b7a328b8d862faa934ed
brant.qin <brant.qin@huolala.cn>
2025-03-31 16:01:00 +0800

    提交编译代码

--- app/makeall.sh
+++ app/makeall.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 #
 # make all cui applications by gcc
+# 在app目录下 执行脚本编译全部文件 makeall.sh 参数1 -f  参数2 makefile 
 #
 
 echo; echo % pos2kml/gcc
 cd pos2kml/gcc
-make $1
+make $1 $2
 cd ../..
 
 echo; echo % str2str/gcc
 cd str2str/gcc
-make $1
+make $1 $2
 cd ../..
 
 echo; echo % rnx2rtkp/gcc
 cd rnx2rtkp/gcc
-make $1
+make $1 $2
 cd ../..
 
 echo; echo % convbin/gcc
 cd convbin/gcc
-make $1
+make $1 $2
 cd ../..
 
 echo; echo % rtkrcv/gcc
 cd rtkrcv/gcc
-make $1
+make $1 $2
 cd ../..
 
